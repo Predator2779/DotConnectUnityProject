4de827cd3786a096ae82b9fd93c1577411191d12
Predator2779 <66646630+Predator2779@users.noreply.github.com>
2024-12-24 21:53:55 +0500

    minor fixes

--- Assets/DotConnect/Scripts/UI/PackListItem.cs
+++ Assets/DotConnect/Scripts/UI/PackListItem.cs
@@ -6,72 +6,72 @@ using UnityEngine.UI;
 
 namespace BizzyBeeGames.DotConnect
 {
-	public class PackListItem : ClickableListItem
-	{
-		#region Inspector Variables
+    public class PackListItem : ClickableListItem
+    {
+        #region Inspector Variables
 
-		[SerializeField] private Text			nameText				= null;
-		[SerializeField] private Text			descriptionText			= null;
-		[SerializeField] private ProgressBar	progressBarContainer	= null;
-		[SerializeField] private Text			progressText			= null;
-		[Space]
-		[SerializeField] private GameObject		lockedContainer			= null;
-		[SerializeField] private GameObject		starsLockedContainer	= null;
-		[SerializeField] private GameObject		iapLockedContainer		= null;
-		[SerializeField] private Text			starAmountText			= null;
-		[SerializeField] private Text			iapText					= null;
+        [SerializeField] private Text nameText = null;
+        [SerializeField] private Text descriptionText = null;
+        [SerializeField] private ProgressBar progressBarContainer = null;
+        [SerializeField] private Text progressText = null;
+        [Space] [SerializeField] private GameObject lockedContainer = null;
+        [SerializeField] private GameObject starsLockedContainer = null;
+        [SerializeField] private GameObject iapLockedContainer = null;
+        [SerializeField] private Text starAmountText = null;
+        [SerializeField] private Text iapText = null;
 
-		#endregion
+        #endregion
 
-		#region Public Variables
+        #region Public Variables
 
-		public void Setup(PackInfo packInfo)
-		{
-			LocalizationManager.Localize(nameText, packInfo.packName);
-			LocalizationManager.Localize(descriptionText, packInfo.packDescription);
-			
-			// nameText.text			= packInfo.packName;
-			// descriptionText.text	= packInfo.packDescription;
+        public void Setup(PackInfo packInfo)
+        {
+            LocalizationManager.Localize(nameText, packInfo.packName);
+            LocalizationManager.Localize(descriptionText, packInfo.packDescription);
 
-			// Check if the pack is locked and update the ui
-			bool isPackLocked = GameManager.Instance.IsPackLocked(packInfo);
+            // nameText.text			= packInfo.packName;
+            // descriptionText.text	= packInfo.packDescription;
 
-			lockedContainer.SetActive(isPackLocked);
-			progressBarContainer.gameObject.SetActive(!isPackLocked);
-			starsLockedContainer.SetActive(isPackLocked && packInfo.unlockType == PackUnlockType.Stars);
-			iapLockedContainer.SetActive(isPackLocked && packInfo.unlockType == PackUnlockType.IAP);
+            // Check if the pack is locked and update the ui
+            bool isPackLocked = GameManager.Instance.IsPackLocked(packInfo);
 
-			if (isPackLocked)
-			{
-				switch (packInfo.unlockType)
-				{
-					case PackUnlockType.Stars:
-						starAmountText.text = "Collect " + packInfo.unlockStarsAmount + " ";
-						break;
-					case PackUnlockType.IAP:
-						SetIAPText(packInfo.unlockIAPProductId);
-						break;
-				}
-			}
-			else
-			{
-				int numLevelsInPack		= packInfo.levelFiles.Count;
-				int numCompletedLevels	= GameManager.Instance.GetNumCompletedLevels(packInfo);
+            lockedContainer.SetActive(isPackLocked);
+            progressBarContainer.gameObject.SetActive(!isPackLocked);
+            starsLockedContainer.SetActive(isPackLocked && packInfo.unlockType == PackUnlockType.Stars);
+            iapLockedContainer.SetActive(isPackLocked && packInfo.unlockType == PackUnlockType.IAP);
 
-				progressBarContainer.SetProgress((float)numCompletedLevels / (float)numLevelsInPack);
-				progressText.text = string.Format("{0} / {1}", numCompletedLevels, numLevelsInPack);
-			}
-		}
+            if (isPackLocked)
+            {
+                switch (packInfo.unlockType)
+                {
+                    case PackUnlockType.Stars:
+                        starAmountText.text = LocalizationManager.Localize("LockedContent.Example.Collect") + " " +
+                                              packInfo.unlockStarsAmount + " ";
+                        break;
+                    case PackUnlockType.IAP:
+                        SetIAPText(packInfo.unlockIAPProductId);
+                        break;
+                }
+            }
+            else
+            {
+                int numLevelsInPack = packInfo.levelFiles.Count;
+                int numCompletedLevels = GameManager.Instance.GetNumCompletedLevels(packInfo);
 
-		#endregion
+                progressBarContainer.SetProgress((float) numCompletedLevels / (float) numLevelsInPack);
+                progressText.text = string.Format("{0} / {1}", numCompletedLevels, numLevelsInPack);
+            }
+        }
 
-		#region Private Methods
+        #endregion
 
-		private void SetIAPText(string productId)
-		{
-			string text = "";
+        #region Private Methods
 
-			#if BBG_IAP
+        private void SetIAPText(string productId)
+        {
+            string text = "";
+
+#if BBG_IAP
 			UnityEngine.Purchasing.Product product = IAPManager.Instance.GetProductInformation(productId);
 
 			if (product == null)
@@ -86,13 +86,13 @@ namespace BizzyBeeGames.DotConnect
 			{
 				text = "Purchase to unlock - " + product.metadata.localizedPriceString;
 			}
-			#else
-			text = "IAP not enabled";
-			#endif
+#else
+            text = "IAP not enabled";
+#endif
 
-			iapText.text = text;
-		}
+            iapText.text = text;
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
